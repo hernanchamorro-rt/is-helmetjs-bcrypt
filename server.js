1ecf7001f2c2bec730e375f4708219f92ed40663
Hernan Chamorro <hernan.chamorro@real-trends.com>
2024-04-13 17:39:02 -0300

    bcrypt.hash to hash a password asynchronous

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const myPlaintextPassword = "sUperpassw0rd!";
 const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /* Store hash in db */
+});
 //END_ASYNC
 
 //START_SYNC
